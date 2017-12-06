5ff5bc5aa4aac1e5abb417da5f00f36931a86881
zaxlct <zaxlct@foxmail.com>
2017-12-06 14:54:31 +0800

    修改 API 域名

--- src/static/js/config.js
+++ src/static/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  baseUrl: 'http://admin-test.huofan.yihunqing.net/admin_api/',
+  baseUrl: 'http://admin-test.huofan.yihunqing.net/app_api/',
   qinniuUrl: 'https://up.qbox.me',
   STATICDOMAIN: 'http://otq0t8ph7.bkt.clouddn.com/',
 }
