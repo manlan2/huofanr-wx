54c8e585c3124033d72123dbfe59e05e37aa8779
zaxlct <zaxlct@foxmail.com>
2017-12-25 11:29:09 +0800

    更改域名

--- src/static/js/config.js
+++ src/static/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  baseUrl: 'http://admin-test.huofan.yihunqing.net/app_api/',
+  baseUrl: 'http://test.huofanr.com/app_api/',
   qinniuUrl: 'https://up.qbox.me',
   STATICDOMAIN: 'http://otq0t8ph7.bkt.clouddn.com/',
 }
