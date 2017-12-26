0063228385f762c65d4e03c93f852cad61eb76b4
zaxlct <zaxlct@foxmail.com>
2017-12-26 16:47:38 +0800

    使用 https

--- src/static/js/config.js
+++ src/static/js/config.js
@@ -1,5 +1,5 @@
 export default {
-  baseUrl: 'http://test.huofanr.com/app_api/',
+  baseUrl: 'https://test.huofanr.com/app_api/',
   qinniuUrl: 'https://up.qbox.me',
   STATICDOMAIN: 'http://otq0t8ph7.bkt.clouddn.com/',
 }
