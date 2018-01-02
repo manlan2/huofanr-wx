cc433612415283c0b9be342d816641c54167c7e6
zaxlct <zaxlct@foxmail.com>
2018-01-02 11:22:24 +0800

    商品分类

--- src/static/js/config.js
+++ src/static/js/config.js
@@ -1,5 +1,6 @@
 export default {
-  baseUrl: 'https://test.huofanr.com/app_api/',
+  // baseUrl: 'https://test.huofanr.com/app_api/',
+  baseUrl: 'http://test.huofanr.com/pc_api/',
   qinniuUrl: 'https://up.qbox.me',
   STATICDOMAIN: 'http://otq0t8ph7.bkt.clouddn.com/',
 }
