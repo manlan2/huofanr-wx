f1c29a912d98c9036c8e032cd57230bef9c7a5ac
zaxlct <zaxlct@foxmail.com>
2018-01-04 11:22:42 +0800

    更换七牛图片路径

--- src/static/js/config.js
+++ src/static/js/config.js
@@ -2,5 +2,5 @@ export default {
   // baseUrl: 'https://test.huofanr.com/app_api/',
   baseUrl: 'http://test.huofanr.com/pc_api/',
   qinniuUrl: 'https://up.qbox.me',
-  STATICDOMAIN: 'http://otq0t8ph7.bkt.clouddn.com/',
+  STATICDOMAIN: 'http://p1wvjiujs.bkt.clouddn.com/',
 }
